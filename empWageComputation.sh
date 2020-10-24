780295c9e3beadfff753fc5b80d971fffccea5f8
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-24 16:46:24 +0530

    Calculated Monthly Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo " Welcome to Employee Wage Computation Program on master Branch"
 
@@ -9,18 +9,19 @@ isFullTime=1
 Wage_Per_Hr=20
 Full_Time_Hr=8
 Part_Time_Hr=4
-#RandomCheck=`expr $((RANDOM%2))`
+WorkingDaysPerMonth=20
 
 if [ $empPresent -eq  $((RANDOM%2)) ]
 then
    echo " Employee is Present"
    RandomCheck=`expr $((RANDOM%2))`
          case $RandomCheck  in
-                1) FullTimeWagePerDay=$(($Wage_Per_Hr * $Full_Time_Hr))
-                        echo " Employee Wage Per Day(FullTime) ="$FullTimeWagePerDay;;
-                0) PartTimeWagePerDay=$(($Wage_Per_Hr * $Part_Time_Hr))
-                        echo " Employee Wage Per Day(PartTime) ="$PartTimeWagePerDay;;
+                1)W_F=$((WorkingDaysPerMonth * Wage_Per_Hr * Full_Time_Hr ))
+                        echo " Employee Wage Per Month(FullTime) ="$W_F;;
+                0)W_P=$((WorkingDaysPerMonth * Wage_Per_Hr * Part_Time_Hr ))
+                        echo " Employee Wage Per Month(PartTime) ="$W_P;;
         esac
 else
    echo " Employee is Absent"
 fi
+
