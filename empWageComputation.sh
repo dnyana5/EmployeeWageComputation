e08e3c00dc6e8d98dc70f8e2c99c0c69573c6c29
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-24 16:30:42 +0530

    Calculated Daily Wage Accordint To Part Time or Full Time Hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,18 +5,24 @@ echo " Welcome to Employee Wage Computation Program on master Branch"
 #To check employee is present or absent
 
 empPresent=1
-
+isFullTime=1
 Wage_Per_Hr=20
-Full_Day_Hr=8
+Full_Time_Hr=8
+Part_Time_Hr=4
 RandomCheck=`expr $((RANDOM%2))`
 
 if [ $empPresent -eq $RandomCheck ]
 then
 
    echo " Employee is Present"
-   #For Calculating Daily Employee Wage
-   Daily_Wage=$(($Wage_Per_Hr * $Full_Day_Hr))
-   echo " Employee Wage Per Day ="$Daily_Wage
+   if [ $isFullTime -eq $((RANDOM%2)) ]  #checking Employee working part time or full time
+   then
+      FullTimeWagePerDay=$(($Wage_Per_Hr * $Full_Time_Hr))
+      echo " Employee Wage Per Day(FullTime) ="$FullTimeWagePerDay
+   else
+      PartTimeWagePerDay=$(($Wage_Per_Hr * $Part_Time_Hr))
+      echo " Employee Wage Per Day(PartTime) ="$PartTimeWagePerDay
+   fi
 else
    echo " Employee is Absent"
 fi
