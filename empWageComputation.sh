32a0a1b1f9fa0aef5588ceabbbae09e403a9ce76
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-24 16:36:21 +0530

    calculating Wage using switch case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo " Welcome to Employee Wage Computation Program on master Branch"
 
@@ -9,20 +9,18 @@ isFullTime=1
 Wage_Per_Hr=20
 Full_Time_Hr=8
 Part_Time_Hr=4
-RandomCheck=`expr $((RANDOM%2))`
+#RandomCheck=`expr $((RANDOM%2))`
 
-if [ $empPresent -eq $RandomCheck ]
+if [ $empPresent -eq  $((RANDOM%2)) ]
 then
-
    echo " Employee is Present"
-   if [ $isFullTime -eq $((RANDOM%2)) ]  #checking Employee working part time or full time
-   then
-      FullTimeWagePerDay=$(($Wage_Per_Hr * $Full_Time_Hr))
-      echo " Employee Wage Per Day(FullTime) ="$FullTimeWagePerDay
-   else
-      PartTimeWagePerDay=$(($Wage_Per_Hr * $Part_Time_Hr))
-      echo " Employee Wage Per Day(PartTime) ="$PartTimeWagePerDay
-   fi
+   RandomCheck=`expr $((RANDOM%2))`
+         case $RandomCheck  in
+                1) FullTimeWagePerDay=$(($Wage_Per_Hr * $Full_Time_Hr))
+                        echo " Employee Wage Per Day(FullTime) ="$FullTimeWagePerDay;;
+                0) PartTimeWagePerDay=$(($Wage_Per_Hr * $Part_Time_Hr))
+                        echo " Employee Wage Per Day(PartTime) ="$PartTimeWagePerDay;;
+        esac
 else
    echo " Employee is Absent"
 fi
